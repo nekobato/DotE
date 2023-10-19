7d4d1f1c9e28ab560d13c5140580348ecf58b860
nekobato <nekobato@gmail.com>
2023-10-19 12:00:00 +0900

    error post

--- components/ErrorPost.vue
+++ components/ErrorPost.vue
@@ -29,7 +29,7 @@ const deleteError = (index: number) => {
         </div> -->
         <div class="hazy-post-contents">
           <div class="hazy-avatar">
-            <Icon icon="mingcute:alert-fill" class="nn-icon" />
+            <Icon icon="mingcute:alert-fill" class="nn-icon size-small" />
           </div>
           <p class="hazy-post-body">
             {{ props.error.message }}
@@ -46,20 +46,6 @@ const deleteError = (index: number) => {
 </template>
 
 <style lang="scss" scoped>
-.username {
-  color: rgba(255, 255, 255, 0.72);
-  font-weight: 600;
-  font-size: var(--font-size-10);
-  font-style: normal;
-  line-height: var(--font-size-10);
-  white-space: nowrap;
-
-  img.emoji {
-    width: var(--font-size-12);
-    height: var(--font-size-12);
-    margin-bottom: -2px;
-  }
-}
 .post-data,
 .repost-data {
   position: relative;
@@ -81,79 +67,9 @@ const deleteError = (index: number) => {
     content: "";
   }
 }
-
-.hazy-post {
-  &.repost {
-    .post-data {
-      .username {
-        display: none;
-      }
-      .hazy-avatar {
-        width: 20px;
-        height: 20px;
-      }
-    }
-    .repost-data {
-      margin-top: -46px;
-      padding-left: 12px;
-      &::before {
-        display: none;
-      }
-      .username {
-        margin-left: -12px;
-      }
-    }
-  }
-}
-.attachments {
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 4px;
-  width: 100%;
-  margin-top: 4px;
-}
-.reactions {
-  display: flex;
-  gap: 4px;
-  width: 100%;
-  margin-top: 4px;
-  overflow-x: scroll;
-  &::-webkit-scrollbar {
-    display: none;
-  }
-  .reaction {
-    display: inline-flex;
-    align-items: center;
-    justify-content: flex-start;
-    height: 24px;
-    padding: 0 2px;
-    background: transparent;
-    border: 1px solid rgba(255, 255, 255, 0.2);
-    border-radius: 4px;
-    &:not(.remote) {
-      cursor: pointer;
-      &:hover {
-        border: 1px solid rgba(255, 255, 255, 0.4);
-      }
-    }
-    &.reacted {
-      background-color: rgba(255, 255, 255, 0.2);
-      border: 1px solid rgba(255, 255, 255, 0.4);
-    }
-    .emoji {
-      height: 20px;
-    }
-    .emoji-default {
-      color: #fff;
-      font-size: 16px;
-      line-height: 20px;
-    }
-    .count {
-      margin-left: 4px;
-      color: #fff;
-      font-size: 12px;
-      line-height: 20px;
-    }
-  }
+.hazy-avatar {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
