c6e63e53af8e506ca9720270ea97f7ebc5ccdf1c
nekobato <nekobato@gmail.com>
2023-11-26 02:22:42 +0900

    remove unused

--- electron/autoupdate.ts
+++ electron/autoupdate.ts
@@ -1,22 +1,16 @@
-import { dialog } from "electron";
 import { autoUpdater } from "electron-updater";
 import log from "electron-log";
 
 export const checkUpdate = () => {
+  autoUpdater.logger = log;
   autoUpdater.checkForUpdatesAndNotify();
 
-  autoUpdater.on("checking-for-update", () => {
-    log.info(process.pid, "checking-for-update...");
-  });
-  // アップデートが見つかった
   autoUpdater.on("update-available", () => {
     log.info(process.pid, "Update available.");
   });
-  // アップデートがなかった（最新版だった）
   autoUpdater.on("update-not-available", () => {
     log.info(process.pid, "Update not available.");
   });
-  // エラーが発生
   autoUpdater.on("error", (err) => {
     log.error(process.pid, err);
   });
