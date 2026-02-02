dd5605bc56a90a4a4bee56d30aa40432eba5fda2
nekobato <nekobato@gmail.com>
2026-02-02 14:27:55 +0900

    fix: validate misskey reply submit

--- src/pages/post/create.vue
+++ src/pages/post/create.vue
@@ -228,7 +228,12 @@ const canSubmit = computed(() => {
     return false;
   }
 
-  if (submitType.value === "note" || submitType.value === "toot" || submitType.value === "post") {
+  if (
+    submitType.value === "note" ||
+    submitType.value === "reply" ||
+    submitType.value === "toot" ||
+    submitType.value === "post"
+  ) {
     if (state.instance?.type === "misskey") {
       return text.value.length > 0 || hasAttachments.value;
     }
